7a074522f42f2bce9e9a1416941aafa1ae8dcd45
Zainab Parveen <zparveen.bsai25seecs@seecs.edu.pk>
2026-09-09 12:31:21 +0500

    test: add additional test cases for add function

--- test_utilities.cpp
+++ test_utilities.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main() {
     cout << "add(2,3) == 5: " << (add(2,3) == 5 ? "PASS" : "FAIL") << endl;
+    cout << "add(-5, -7) == -10: " << (add(-5, -5) == -10 ? "PASS" : "FAIL") << endl;
+    cout << "add(5, 15) == 20: " << (add(-5, -5) == -10 ? "PASS" : "FAIL") << endl;
     return 0;
 }
